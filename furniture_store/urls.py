873dc9c610602f6089ecafebcc394e21e7751649
MbongeniChristian3 <mbongenichristian3@gmail.com>
2025-04-04 02:30:22 -0700

    updated urls

--- furniture_store/urls.py
+++ furniture_store/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+
+def redirect_to_products(request):
+    return redirect('products:index')  # Ensure 'index' is a valid URL name in 'products.urls'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', include('products.urls')),
-    path('', redirect_to_products),# Make sure this line is correct
+    path('', redirect_to_products),
+    path('', redirect_to_products),
 ]
