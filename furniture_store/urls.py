c5ab11428cbb9f76bbd0a00c1d443bb4a4f6f4d9
MbongeniChristian3 <mbongenichristian3@gmail.com>
2025-04-07 01:23:26 -0700

    index update

--- furniture_store/urls.py
+++ furniture_store/urls.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
 
-def redirect_to_product_list(request):
-    return redirect('products:product_list')  # Ensure 'index' is a valid URL name in 'products.urls'
+def redirect_to_index(request):
+    return redirect('products:index')  # Ensure 'index' is a valid URL name in 'products.urls'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', include('products.urls')),
-    path('', redirect_to_product_list),
+    path('', redirect_to_index),
 ]
--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import product_list, product_detail, product_create, product_update, product_delete
 
-urlpatterns = [
+urlpatterns = [    
     path('', product_list, name='product_list'),
     path('<int:pk>/', product_detail, name='product_detail'),
     path('create/', product_create, name='product_create'),
